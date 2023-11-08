b8dd3efcfce503b680d4e94ec7bb147b911b9bfe
Akbar Hossain <hakbar431@gmail.com>
2023-11-08 21:14:06 +0600

    creating another database collection

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ async function run() {
             res.send(result)
         })
 
+        
         app.patch('/jobs/:id', async (req, res) => {
             const id = req.params.id
             const filter = { _id: new ObjectId(id) }
