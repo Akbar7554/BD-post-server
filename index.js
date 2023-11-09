b87e788f2914c54317bfc36f15fa6ba21ba842a5
Akbar Hossain <hakbar431@gmail.com>
2023-11-09 17:04:27 +0600

    deploy issue

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+}
+))
 app.use(express.json())
-
+app.use(cookieParser())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1i934d1.mongodb.net/?retryWrites=true&w=majority`;
@@ -20,6 +26,26 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+
+// middlewares
+const logger = (req, res, next) => {
+    console.log(req.method, req.url)
+    next()
+}
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token
+    console.log('token in middleware', token)
+    if (!token) {
+        return res.status(401).send({ message: "Unauthorized access" })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({message : 'unauthorized access'})
+        }
+        req.user = decoded
+        next()
+    })
+}
 
 async function run() {
     try {
@@ -86,17 +112,83 @@ async function run() {
             const result = await bidCollection.insertOne(newBid)
             res.send(result)
         })
-
-        app.get('/bids', async (req, res) => {
+        // userEmail wise bid
+        app.get('/bids', logger, verifyToken, async (req, res) => {
+            console.log(req.query.email);
+            console.log('token owner info', req.user)
             let query = {}
             if (req.query?.email) {
-                query = { email: req.query.email }
+                query = { userEmail: req.query.email }
             }
-            const result = await bidCollection.find().toArray()
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({message: "forbidden access"})
+            }
+            const result = await bidCollection.find(query).toArray()
+            res.send(result)
+        })
+        // buyerEmail wise bid
+        app.get('/bids/buyerEmail', logger, verifyToken, async (req, res) => {
+            console.log(req.query.email);
+            console.log('token owner info', req.user)
+            let query = {}
+            if (req.query?.email) {
+                query = { buyerEmail: req.query.email }
+            }
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({message: "forbidden access"})
+            }
+            const result = await bidCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get('/bids/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await bidCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.patch('/bids/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updateBids = req.body
+            console.log(updateBids)
+            const updateDoc = {
+                $set: {
+                    status: updateBids.status
+                }
+            }
+            const result = await bidCollection.updateOne(filter, updateDoc)
             res.send(result)
         })
 
 
+        // jwt auth related api
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            console.log(user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            // res.cookie('token', token, {
+            //     httpOnly: true,
+            //     secure: false,
+            //     sameSite: 'none'
+            // })
+            //     .send({ success: true })
+                res.cookie('token', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+
+                })
+                    .send({ success: true })
+        })
+
+        app.post('/logout', async (req, res) => {
+            const user = req.body
+            console.log('logged out', user);
+            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+        })
 
 
 
