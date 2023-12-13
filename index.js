71939e09911d8a36d698aa5a3a062e71fc997a1c
Akbar Hossain <hakbar431@gmail.com>
2023-12-13 18:42:24 +0600

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000
 app.use(cors({
     origin: [
         // 'http://localhost:5173',
-        'https://bd-post-e405e.web.app',
+        'https://bd-post-server.vercel.app',
         'https://bd-post-e405e.firebaseapp.com'
     ],
     credentials: true,
@@ -199,7 +199,7 @@ async function run() {
 
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
